419af3d31865461202d11f9fd591b41f27643d66
ginglis13 <ginglis05@gmail.com>
2021-06-04 21:38:36 -0400

    some work / skeletoning on single.c

---
+++ src/single.rs
@@ -0,0 +1,23 @@
+// single.rs
+// for handling single HTTP connection
+
+use std::net::TcpListener;
+use crate::request::Request;
+
+pub fn single_server(stream: &TcpListener, root_path: String) {
+    /* Accept and handle HTTP request */
+    loop {
+    	/* Accept request */
+        let mut r = Request::new();
+
+	    /* Handle request */
+        r.handle_request(root_path.to_string());
+    }
+
+    /* Close server socket */
+	// this will happen automatically as Rust will drop the TcpListener once it is
+    // out of scope
+
+    //return EXIT_SUCCESS;
+    // return a result most likely
+}
