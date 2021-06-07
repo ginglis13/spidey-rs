9f1202c85345d3bbf4584ec266bf29d0fd462e79
ginglis13 <ginglis05@gmail.com>
2021-06-07 16:12:29 -0400

    handle_browse_request: mostly complete, more testing/error handling/arch needed

--- src/request.rs
+++ src/request.rs
@@ -130,8 +130,62 @@ impl Request {
         Ok(())
     }
     
-    fn handle_browse_request(&self) {
+    fn handle_browse_request(&self) -> Result<(), Box<dyn Error>> {
+        let dir = fs::read_dir(self.path.to_str().unwrap()).unwrap(); // Returns option -> make fn signature option??
 
+        let mut stream = self.tcp_stream.as_ref().unwrap();
+
+        // Write HTTP Header with OK Status and text/html Content-Type
+        stream.write(&"HTTP/1.0 200 OK\r\n".as_bytes())?;
+        stream.write(&"Content-Type: text/html\r\n".as_bytes())?;
+        stream.write(&"\r\n".as_bytes())?;
+
+        // Head tag
+        stream.write(&"<head>\n".as_bytes());
+        stream.write(&"<link href=\"css/bootstrap.min.css\" rel=\"stylesheet\">".as_bytes())?;
+        stream.write(&"<link href=\"css/custom.css\" rel=\"stylesheet\">".as_bytes())?;
+        stream.write(&"</head>\n".as_bytes())?;
+
+        // Website heading
+        stream.write(&"<body>\n".as_bytes())?;
+        stream.write(&"<div class=\"jumbotron\">\n".as_bytes())?;
+        stream.write(&"<div class=\"container\">\n".as_bytes())?;
+        stream.write(&"<h1 style=\"text-align: center; color: white\">Gourd Experts</h1>".as_bytes())?;
+        stream.write(&"<h4 style=\"text-align: center; color: white\">Burger, Gallahue, Inglis</h4>".as_bytes())?;
+        stream.write(&"</div>\n".as_bytes())?;
+        stream.write(&"</div>\n".as_bytes())?;
+
+        // For each entry in directory, emit HTML list item
+        stream.write(&"<div class=\"list-group container\">\n".as_bytes())?;
+
+        for entry in dir {
+            let entry = entry.unwrap();
+            let entry_str = entry.path();
+            let entry_name = entry.file_name();
+
+            log::info!("Path: {}", entry_str.display());
+            log::info!("Name: {:?}", entry_name);
+
+            // Create the href
+            stream.write(&"<a href=".as_bytes())?;
+
+            // Check root dir and relative dirs
+            if self.uri == String::from("/") {
+                stream.write(&"/".as_bytes())?;
+            }
+
+            // TODO: href contains quotes
+            // Add path to href, close href, add label
+            stream.write(format!("{} ", entry_name.to_str().unwrap()).as_bytes())?;
+            stream.write(&"class=\"list-group-item list-group-item-action\">".as_bytes())?;
+            stream.write(format!("{:?}", entry_name).as_bytes())?;
+            stream.write(&"</a>".as_bytes())?;
+        }
+
+        stream.write(&"</div>\n".as_bytes())?;
+        stream.write(&"</body>\n".as_bytes())?;
+
+        Ok(())
     }
 
     fn handle_file_request(&self) {
