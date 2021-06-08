b18d97b60896a74b80f7300ed3decadc649040a9
ginglis13 <ginglis05@gmail.com>
2021-06-07 23:06:08 -0400

    handle_error and handle_file_request roughly done
    Both handle_error and handle_file_request having basic functionality
    implemented, but there are plenty of TODOs in comments as far as
    some error handling, borrowing issues, etc. goes.

--- src/request.rs
+++ src/request.rs
@@ -2,10 +2,20 @@
 // contains struct definitions and implementations for handling HTTP headers / requests
 
 use std::fs;
+use std::fs::File;
 use std::error::Error;
 use std::path::PathBuf;
 use std::io::prelude::*;
+use std::io::Read;
 use std::collections::LinkedList;
+
+#[derive(Copy, Clone, PartialEq)]
+enum HttpStatus {
+    OK,
+    BAD_REQUEST,
+    NOT_FOUND,
+    INTERNAL_SERVER_ERROR,
+}
 
 #[derive(Debug)]
 pub struct Header {
@@ -53,19 +63,27 @@ impl Request {
         self.parse_request(); /* TODO: if error, return HTTP_STATUS_BAD_REQUEST */
 
         match self.determine_request_path(root_path) {
-            Ok(_) => log::info!("Request to path {:?}", self.path),
-            Err(e) => log::error!("Bad path given: {}", e), /* TODO: return HTTP_STATUS_BAD_REQUEST status error code in this case */
+            Ok(_) => {
+                log::info!("Request to path {:?}", self.path);
+                // Determine filetype. PathBuf type kinda does it for us!
+                if self.path.is_dir() {
+                    self.handle_browse_request();
+                } else if self.path.is_file() {
+                    self.handle_file_request();
+                } else {
+                    /* HTTP_STATUS_BAD_REQUEST */
+                    self.handle_error(HttpStatus::BAD_REQUEST);
+                    return;
+                }
+            },
+            Err(e) => {
+                log::error!("Bad path given: {}", e); 
+                self.handle_error(HttpStatus::NOT_FOUND);
+                return;
+            },
         }
 
-        // Determine filetype. PathBuf type kinda does it for us!
-        if self.path.is_dir() {
-            self.handle_browse_request();
-        } else if self.path.is_file() {
-            self.handle_file_request();
-        } else {
-            /* HTTP_STATUS_BAD_REQUEST */
-            self.handle_error();
-        }
+        
     }
 
     fn determine_request_path(&mut self, root_path: String) -> Result<(), Box<dyn Error>> {
@@ -178,7 +196,7 @@ impl Request {
             // Add path to href, close href, add label
             stream.write(format!("{} ", entry_name.to_str().unwrap()).as_bytes())?;
             stream.write(&"class=\"list-group-item list-group-item-action\">".as_bytes())?;
-            stream.write(format!("{:?}", entry_name).as_bytes())?;
+            stream.write(format!("{}", entry_name.to_str().unwrap()).as_bytes())?;
             stream.write(&"</a>".as_bytes())?;
         }
 
@@ -188,13 +206,109 @@ impl Request {
         Ok(())
     }
 
-    fn handle_file_request(&self) {
+    fn handle_file_request(&mut self) -> Result<(), Box<dyn Error>> {
+
+        let mut stream = self.tcp_stream.as_ref().unwrap();
+
+        // TODO: This will panic if the file contents are not valid UTF-8 !!
+        //let file_contents = fs::read_to_string(self.path.to_str().unwrap()).unwrap();
+
+        /* Determine mimetype */
+        //let mimetype = determine_mimetype(self.path);
+        // TODO: Actually determine mimetype - for now, text/plain
+        let mimetype = String::from("text/plain");
+
+        /* Write HTTP Headers with OK status and determined Content-Type */
+        stream.write(&"HTTP/1.0 200 OK\n".as_bytes());
+        stream.write(&"Content-Type: ".as_bytes());
+        stream.write(format!("{}\n", mimetype).as_bytes());
+        stream.write(&"\r\n".as_bytes());
+
+        let mut file = File::open(self.path.to_str().unwrap())?;
+        
+        let mut buffer = [0; 4096];
+
+        loop {
+            match file.read(&mut buffer) {
+                Ok(nread) => {
+                    if nread == 0 {
+                        break; 
+                    }
+                    // Check that bytes read = bytes written ??
+                    let nwritten = stream.write(&buffer).unwrap();
+
+                    if nread != nwritten {
+                        // TODO: this handle_error is a mutable borrow. Cannot occur due to
+                        // immutable borrows referencing stream
+                        // self.handle_error(HttpStatus::INTERNAL_SERVER_ERROR);
+                        // e not in scope (duh) -> just return Ok() since handle_error displays
+                        // 500 page? That would differ from below Err(e) case below...
+                        // return Err(Box::new(x)); 
+                    }
+                },
+                Err(e) => { // Read error can occur 
+                    self.handle_error(HttpStatus::INTERNAL_SERVER_ERROR);
+                    return Err(Box::new(e));
+                },
+
+            }  
+    
+
+        }
+        Ok(())
 
     }
 
-    fn handle_error(&mut self) {
+    fn handle_error(&mut self, status: HttpStatus) {
+        let status_string = http_status_string(status);
+        
+        let mut stream = self.tcp_stream.as_ref().unwrap();
 
+        /* Write HTTP Header */
+        stream.write(format!("HTTP/1.0 {}\r\n", status_string).as_bytes());
+        stream.write(&"Content-Type: text/html\r\n".as_bytes());
+        stream.write(&"\r\n".as_bytes());
+    
+        // Head tag
+        stream.write(&"<head>\n".as_bytes());
+        stream.write(&"<link href=\"css/bootstrap.min.css\" rel=\"stylesheet\">".as_bytes());
+        stream.write(&"<link href=\"css/custom.css\" rel=\"stylesheet\">".as_bytes());
+        stream.write(&"</head>\n".as_bytes());
+    
+    
+        // Website heading
+        stream.write(&"<body>\n".as_bytes());
+        stream.write(&"<div class=\"jumbotron\">\n".as_bytes());
+        stream.write(&"<div class=\"container\">\n".as_bytes());
+        stream.write(&"<h1 style=\"text-align: center; color: white\">Gourd Experts</h1>".as_bytes());
+        stream.write(&"<h4 style=\"text-align: center; color: white\">Burger, Gallahue, Inglis</h4>".as_bytes());
+        stream.write(&"</div>\n".as_bytes());
+        stream.write(&"</div>\n".as_bytes());
+    
+        /* Write HTML Description of Error*/
+        log::info!("STATUS STR: {}", status_string);
+        stream.write(format!("<h1>{}</h1>", status_string).as_bytes());
+
+        // Tbt to when this sophomore yr proj was gourd themed.
+        if status == HttpStatus::NOT_FOUND {
+            stream.write(&"<p>The gourd you seek could not be foud. Do not give up hope, there are plenty of gourds in the sea.</p>".as_bytes());
+        }
+        else if status == HttpStatus::BAD_REQUEST {
+           stream.write(&"<p>Bad gourd request.</p>".as_bytes());
+        }
+        else if status == HttpStatus::INTERNAL_SERVER_ERROR {
+           stream.write(&"<p>Something went wrong. Contact gourdhelp@nd.edu for assistance.</p>".as_bytes());
+        }
+    
+        stream.write(&"</body>".as_bytes());
     }
-
 }
 
+fn http_status_string(status: HttpStatus) -> String {
+    match status {
+        HttpStatus::OK => String::from("200 OK"),
+        HttpStatus::BAD_REQUEST => String::from("400 Bad Request"),
+        HttpStatus::NOT_FOUND => String::from("404 Not Found"),
+        HttpStatus::INTERNAL_SERVER_ERROR => String::from("500 Internal Server Error"),    
+    }
+}
